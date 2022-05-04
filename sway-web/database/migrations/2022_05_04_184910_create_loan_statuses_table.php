7e5dd6f2ee53738fd62c829265568c269a7acff9
Moha - DAM2 <moha@26814FD>
2022-05-04 20:49:42 +0200

    Controlador y migracion re rehehcas para el loan status

--- sway-web/database/migrations/2022_05_04_184910_create_loan_statuses_table.php
+++ sway-web/database/migrations/2022_05_04_184910_create_loan_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class LoanStatus extends Migration
+class CreateLoanStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class LoanStatus extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('loan_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class LoanStatus extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('loan_statuses');
     }
 }

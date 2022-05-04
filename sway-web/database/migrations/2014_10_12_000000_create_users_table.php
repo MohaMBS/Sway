fa95a8dd8fa38afa05ed112bd70b23e25ae2be94
Moha - DAM2 <moha@26814FD>
2022-05-04 20:08:03 +0200

    Modificando las migraciones

--- sway-web/database/migrations/2014_10_12_000000_create_users_table.php
+++ sway-web/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,13 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('company_id')->nullable();
+            $table->boolean('is_public')->default(1);
+            $table->boolean('is_baned')->default(0);
+            $table->boolean('is_admin')->default(0)
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ sway-web/database/migrations/2022_05_03_193541_create_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_to_id');
+            $table->integer('user_from_id');
+            $table->integer('connection_status_id');
+            $table->timestamps();
+
+            //referencias a la tabla de usuarios para la relacion
+            $table->foreign('user_to_id')->references('id')->on('users');
+            $table->foreign('user_from_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}

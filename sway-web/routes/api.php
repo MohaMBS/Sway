2094d029c2251333e125665a089eff368da6eb9f
Moha - DAM2 <moha@26814FD>
2022-05-09 20:30:05 +0200

    Creando las primeras rutas de la api

--- sway-web/routes/api.php
+++ sway-web/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PassportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,32 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('register', [UserController::class, 'store'])
+       ->name('user.store');
+
+Route::middleware('auth:api')->group(function (){
+    Route::post('test', function(Request $request){
+        return $request->a;
+    });
+});*/
+
+//Metodo para darse de alta
+Route::post('register', [PassportController::class, 'register'])->name('passport.register');
+Route::get('login', [PassportController::class, 'login'])->name('passport.login');
+
+Route::group(['middleware' => 'auth:api'], function() {
+    // Metodo para cerrar session en el dispositivo
+    Route::get('logout', [PassportController::class, 'logout'])->name('passport.logout');
+
+    Route::get('user/info', [UserController::class, 'userDetail'])->name('passport.userInfo');
+
+    Route::get('test',function(Request $request){
+        return $request->a;
+    })->name('passport.test');
+
 });

7e1b4758fc45042094960a0912b647b69a27c381
Moha - DAM2 <moha@26814FD>
2022-05-09 20:24:54 +0200

    Haciendo las primeras relaciones de los modelos de BBDD

--- sway-web/app/Models/User.php
+++ sway-web/app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -37,4 +38,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * Relacion con la compañia
+     */
+    public function company(){
+        return $this->hasOne(Company::class);
+    }
+
+    public function contacts(){
+        return $this->hasMany(Contact::class,'user_from_id','id')->where('connection_status_id','=',0);
+    }
 }
